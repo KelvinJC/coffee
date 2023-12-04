6a8bf65c5705480544f7c0caeb8a12cda75cdbbb
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2023-12-04 14:27:57 +0100

    Remove unused imports

--- lib/util/coffee_tile.dart
+++ lib/util/coffee_tile.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:coffee_app/models/models.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class CoffeeTile extends StatelessWidget {
